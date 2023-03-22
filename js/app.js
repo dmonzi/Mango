196a32baaf9859982c7ac5861e61e2dc6a38e13c
dmonzi <danielmonzonblazquez13@gmail.com>
2023-03-22 08:58:14 +0100

    fist commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/* Signup ----------------------------------------------*/
+
+function validarContraseña() {
+    let input, contraseña, longitud, mayuscula, hasNumero;
+
+    longitud, mayuscula, hasNumero = false;
+    input = document.getElementById('passwd');
+    contraseña = input.value;
+
+    // Comprobar longitud
+    if (contraseña.lenght >= 8) {
+        hasNumero = true;
+    }
+
+    // Ver mayus
+    for (let i = 0; i < contraseña.lenght; i++) {
+         
+    }
+
+    // Ver numero
+    for (let i = 0; i < contraseña.lenght; i++) {
+        
+    }
+
+    
+    
+}
+
+
+
+
+
+
+
