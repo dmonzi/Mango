d4c5778440370d990860ae4705e4104267cc1b4b
dmonzi <danielmonzonblazquez13@gmail.com>
2023-03-27 12:39:02 +0200

    03-27-23

--- js/app.js
+++ js/app.js
@@ -22,13 +22,38 @@ function validarContraseña() {
         
     }
 
-    
-    
+}
+
+function addPopUp(array){
+    let popUpContainer, popUpContent;
+    let divPopUp;
+    for(let puntosTweet of array){
+        //Aqui lo que hago es crear los elementos para cada post
+        popUpContainer=document.createElement('div');
+        popUpContainer.classList.add('popUp');
+        popUpContent=document. createElement('p');
+        popUpContent.textContent='Seguir a diego';
+        popUpContainer.appendChild(popUpContent);
+        puntosTweet.appendChild(popUpContainer);
+
+        //Pongo a escucha del evento click a todos los elementos
+        //con la clase ptos, que son los tres puntos que deben 
+        //mostrar el menu que acabo de crear
+        puntosTweet.addEventListener('click',(event)=>{
+            event.target.classList.toggle('seleccionado');
+            divPopUp=event.target.nextElementSibling;
+            if(divPopUp.style.display=='block'){
+                divPopUp.style.display='none';
+            }else{
+            divPopUp.style.display='block';
+            }
+        });
+    }
 }
 
 
 
-
-
-
-
+window.addEventListener("DOMContentLoaded", (event) => {
+    let puntos=document.getElementsByClassName('ptos');
+    addPopUp(puntos);
+});
--- js/users.js
+++ js/users.js
@@ -1,8 +1,31 @@
+
+let li, a, i;
+
 let user={
     name:"pipo",
     username:"popiopo",
     email:"pipo@gmail.com",
     password:"123123",
     pfp:"./images/cafe.jpg",
-    admin:true
+    admin: true
 }
+
+function verAdmin() {
+    if (user.admin) {
+        li = document.getElementById('last-li');
+
+        a = document.createElement('a');
+        i = document.createElement('i');
+
+        // <i class="fa-solid fa-screwdriver-wrench"></i>
+        i.classList.add('fa-solid', 'fa-screwdriver-wrench');
+        a.setAttribute('href', 'admin.html');
+        a.classList.add('admin-btn');
+
+        a.appendChild(i);
+        li.appendChild(a);
+    }
+}
+
+
+verAdmin();
